3bec37f5108da5f0aab2d2c1fc5a7f06bdcdb265
karrakunga <karrakunga@gmail.com>
2016-02-18 16:56:38 +0000

    Removed test data

--- game.js
+++ game.js
@@ -1,24 +1,8 @@
 var neighbourCounter = require('./neighbourCounter');
 
-var test = 
-        [
-            ['.','.','.','.'],
-            ['.','.','.','.'],
-            ['.','*','.','.'],
-            ['.','.','.','.'],
-            ['.','.','.','.'],
-            ['.','.','.','.']
-        ]
-
-var cellCounts = neighbourCounter.countNeighbours(test);
-
-console.log(cellCounts);
-
-
 module.exports = {
     run: function(cells){
-        
-        
+       
         return [
             [".",".",".","."],
             [".",".",".","."],
