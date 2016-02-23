1a9d5479c049d4fe66f81ae4d7eb6edceab05f8f
karrakunga <karrakunga@gmail.com>
2016-02-23 09:09:07 +0000

    Initial Api Commit

---
+++ server/routes/helloWorld.js
@@ -0,0 +1,9 @@
+module.exports = function(router) {
+    router.get('/HelloWorld', function(req, res){
+       res.json({message: 'Hello World'});
+    }),
+    router.post('/HelloWorld', function(req, res){
+          console.log("POST RECEIVED");
+          res.send("Ta");
+    });
+};
