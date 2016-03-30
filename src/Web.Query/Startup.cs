d1a42220d678495a2f5d4e4334e77d128510a314
karrakunga <karrakunga@gmail.com>
2016-03-30 08:20:49 +0100

    Seperated startup class

---
+++ src/Web.Query/Startup.cs
@@ -0,0 +1,31 @@
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace Web.Query
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+                
+            config.Services.Replace(typeof(IHttpControllerActivator), new StructureMapServiceActivator());
+
+            config.Routes.MapHttpRoute("Default", "{controller}/{id}",new { id = RouteParameter.Optional });
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings =
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
