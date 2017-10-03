9840b7e53b50e3470dd2c9306b7f634fc586464f
Scott Lyons <karrakunga@gmail.com>
2017-10-03 09:44:58 +0100

    Added tslint

--- src/game.ts
+++ src/game.ts
@@ -3,20 +3,20 @@ class Game {
     constructor(private neighbourCounter: counters.INeighbourCounter) {}   
     
     public startGame() {
-            let inputElements: HTMLInputElement[][] = this.getCellInputElements();
-        
-            let neighbourCountGrid = this.neighbourCounter.countNeighbours(inputElements);
+        let inputElements: HTMLInputElement[][] = this.getCellInputElements();
+    
+        let neighbourCountGrid = this.neighbourCounter.countNeighbours(inputElements);
 
-            for(let row = 0; row < enums.CELL_COUNT_X; row++) {
-                for(let col = 0; col < enums.CELL_COUNT_Y; col++) {
-                    let neighbourCount = neighbourCountGrid[row][col];            
-                    let cell = inputElements[row][col];
-                    cell.checked = this.checkForUnderpopulation(neighbourCount, cell);
-                    cell.checked = this.checkForOvercrowding(neighbourCount, cell);
-                    cell.checked = this.checkForMating(neighbourCount, cell);
-                }
+        for(let row = 0; row < enums.CELL_COUNT_X; row++) {
+            for(let col = 0; col < enums.CELL_COUNT_Y; col++) {
+                let neighbourCount = neighbourCountGrid[row][col];            
+                let cell = inputElements[row][col];
+                cell.checked = this.checkForUnderpopulation(neighbourCount, cell);
+                cell.checked = this.checkForOvercrowding(neighbourCount, cell);
+                cell.checked = this.checkForMating(neighbourCount, cell);
             }
         }
+    }
 
     private getCellInputElements(): HTMLInputElement[][] {
         let inputElements: HTMLInputElement[][] = [];
--- src/neighbourCounter.ts
+++ src/neighbourCounter.ts
@@ -5,25 +5,25 @@ namespace counters {
         countNeighbours(cells: HTMLInputElement[][]): number[][];
     }
 
-    export class NeighbourCounter implements INeighbourCounter {                
+    export class NeighbourCounter implements INeighbourCounter {
         public countNeighbours(cells: HTMLInputElement[][]): number[][] {
             let cellCounts: number[][] = [];
-            
+
             for(let row = 0; row < cells.length; row++) {
                 cellCounts.push([]);
-                
-                for(let col = 0; col < cells[row].length; col++) {                
+
+                for(let col = 0; col < cells[row].length; col++) {
 
                     let count: number = 0;
-                    if(this.hasLiveCell(row - 1, col - 1, cells)) count++;                         
-                    if(this.hasLiveCell(row, col - 1, cells)) count++; 
-                    if(this.hasLiveCell(row + 1, col - 1, cells)) count++; 
-                    if(this.hasLiveCell(row - 1, col, cells)) count++; 
-                    if(this.hasLiveCell(row + 1, col, cells)) count++; 
-                    if(this.hasLiveCell(row - 1, col + 1, cells)) count++; 
-                    if(this.hasLiveCell(row, col + 1, cells)) count++; 
-                    if(this.hasLiveCell(row + 1, col + 1, cells)) count++; 
-                                    
+                    if(this.hasLiveCell(row - 1, col - 1, cells)) count++;
+                    if(this.hasLiveCell(row, col - 1, cells)) count++;
+                    if(this.hasLiveCell(row + 1, col - 1, cells)) count++;
+                    if(this.hasLiveCell(row - 1, col, cells)) count++;
+                    if(this.hasLiveCell(row + 1, col, cells)) count++;
+                    if(this.hasLiveCell(row - 1, col + 1, cells)) count++;
+                    if(this.hasLiveCell(row, col + 1, cells)) count++;
+                    if(this.hasLiveCell(row + 1, col + 1, cells)) count++;
+
                     cellCounts[row][col] = count;
                 }
             }
@@ -35,7 +35,7 @@ namespace counters {
             if(!cells[row]) return enums.DEAD_CELL;
             if(!cells[row][col]) return enums.DEAD_CELL;
             if(cells[row][col].checked != enums.LIVE_CELL) return enums.DEAD_CELL;
-            
+
             return enums.LIVE_CELL;
         }
     }
